ae0df99caff081db959c0385f98f2e319153ba56
Vukman György <gyorgy.vukman@nokia.com>
2019-09-13 15:56:08 +0200

    Wizard is now working with dynamic content.

--- client/view/FirstStartComponents.py
+++ client/view/FirstStartComponents.py
@@ -1,7 +1,7 @@
 from enum import IntEnum
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
-from PyQt5.QtCore import QSettings, Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
+from PyQt5.QtCore import QSettings, Qt, pyqtSlot
 from PyQt5 import QtCore
 from PyQt5.QtGui import QColor, QPainter, QFont, QPen, QPixmap
 
@@ -15,17 +15,84 @@ class FirstStartWizard(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__settings = QSettings()
+        self.__widgetMap = {
+            WizardProgressWidget.WIZARD_PROGRESS_STATES.WELCOME: WelcomeWidget(),
+            WizardProgressWidget.WIZARD_PROGRESS_STATES.NETWORK: SetupNetworkWidget(),
+            WizardProgressWidget.WIZARD_PROGRESS_STATES.ACCOUNTS: WelcomeWidget(),
+            WizardProgressWidget.WIZARD_PROGRESS_STATES.SUMMARY: SetupNetworkWidget(),
+        }
         self.__setup()
 
     def __setup(self):
+        self.__state = WizardProgressWidget.WIZARD_PROGRESS_STATES.WELCOME
+        self.__setupStyle()
+        self.__setupWidgets()
+
+    def __setupStyle(self):
+        self.setAttribute(Qt.WA_StyledBackground)
+        self.setStyleSheet(
+        """
+            QPushButton {
+                background-color:#e36410;
+                color:white;
+                width:150px;
+                border:0px;
+                height:30px;
+            }
+            QPushButton:disabled {
+                background-color:#D8D8D8;
+            }
+        """)
+
+    def __setupWidgets(self):
         self.__layout = QVBoxLayout()
         self.__layout.setContentsMargins(0, 0, 0, 0)
         self.__progressWidget = WizardProgressWidget()
-        self.__welcomeWidget = WelcomeWidget()
+        self.__setupNetworkWidget = SetupNetworkWidget()
         self.__layout.addWidget(self.__progressWidget, 0, Qt.AlignTop)
-        self.__layout.addWidget(self.__welcomeWidget, 0, Qt.AlignTop)
-        self.__layout.addStretch(1)
+
+        for key, widget in self.__widgetMap.items():
+            self.__layout.addWidget(widget, 0, Qt.AlignTop)
+            widget.show() if key == WizardProgressWidget.WIZARD_PROGRESS_STATES.WELCOME else widget.hide()
+
+        self.__nextButton = QPushButton("Next")
+        self.__nextButton.clicked.connect(self.__goNext)
+        self.__previousButton = QPushButton("Back")
+        self.__previousButton.setDisabled(True)
+        self.__previousButton.clicked.connect(self.__goBack)
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(0, 0, 20, 20)
+        controlLayout.setSpacing(20)
+        controlLayout.setAlignment(Qt.AlignTrailing)
+        controlLayout.addWidget(self.__previousButton)
+        controlLayout.addWidget(self.__nextButton)
+
+        self.__layout.addLayout(controlLayout)
         self.setLayout(self.__layout)
+
+    @pyqtSlot()
+    def __goNext(self):
+        if self.__state != WizardProgressWidget.WIZARD_PROGRESS_STATES.SUMMARY:
+            self.__previousButton.setDisabled(False)
+            self.__widgetMap[self.__state].hide()
+            self.__state = self.__progressWidget.toNextState()
+            self.__widgetMap[self.__state].show()
+            if self.__state == WizardProgressWidget.WIZARD_PROGRESS_STATES.SUMMARY:
+                self.__nextButton.setDisabled(True)
+            self.__progressWidget.update()
+            self.update()
+
+    @pyqtSlot()
+    def __goBack(self):
+        if self.__state == WizardProgressWidget.WIZARD_PROGRESS_STATES.SUMMARY:
+            self.__nextButton.setDisabled(False)
+        self.__widgetMap[self.__state].hide()
+        self.__state = self.__progressWidget.toPreviousState()
+        self.__widgetMap[self.__state].show()
+        if self.__state == WizardProgressWidget.WIZARD_PROGRESS_STATES.WELCOME:
+            self.__previousButton.setDisabled(True)
+        self.__progressWidget.update()
+        self.update()
 
 
 class WizardProgressWidget(QWidget):
@@ -39,16 +106,12 @@ class WizardProgressWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__state = WizardProgressWidget.WIZARD_PROGRESS_STATES.WELCOME
-        self.__ENUM_DISPLAY_VALUES = ['Welcome', 'Setup SSH', 'Accounts', 'Summary']
         self.__activeStateColor = QColor('#E39910')
         self.__inactiveStateColor = QColor('#D8D8D8')
         self.__separatorLineColor = QColor('#777777')
         self.__stageIndexFont = QFont('Arial', 32, QFont.Bold, False)
         self.__stageLabelFont = QFont('Arial', 12, QFont.Bold, False)
-        self.__pen = QPen(self.__activeStateColor, 6, Qt.SolidLine)
         self.setFixedSize(1280, 160)
-        # self.setAttribute(Qt.WA_StyledBackground)
-        # self.setStyleSheet("background:#151515;")
 
     def paintEvent(self, e):
         qp = QPainter()
@@ -61,80 +124,112 @@ class WizardProgressWidget(QWidget):
         self.__drawSeparatorLine(painter)
 
     def __drawProgressIcons(self, painter):
+        pen = QPen(self.__activeStateColor, 6, Qt.SolidLine)
         for state in self.WIZARD_PROGRESS_STATES:
             posX = (state.value * 320) + 120
             posY = 20
             width = 80
             height = 80
             if state <= self.__state:
-                self.__pen.setColor(self.__activeStateColor)
+                pen.setColor(self.__activeStateColor)
             else:
-                self.__pen.setColor(self.__inactiveStateColor)
-            painter.setPen(self.__pen)
+                pen.setColor(self.__inactiveStateColor)
+            painter.setPen(pen)
             painter.setFont(self.__stageIndexFont)
             painter.drawRect(posX, posY, width, height)
             painter.drawText(QtCore.QRect(posX, posY, width, height), Qt.AlignCenter, str(state.value + 1))
             painter.setFont(self.__stageLabelFont)
-            painter.drawText(QtCore.QRect(posX, posY + 90, width, 25), Qt.AlignCenter, self.__ENUM_DISPLAY_VALUES[state.value])
+            painter.drawText(QtCore.QRect(posX, posY + 90, width, 25), Qt.AlignCenter, state.toDisplayValue())
             if state > self.WIZARD_PROGRESS_STATES.WELCOME:
                 painter.drawLine(posX - 6, posY + (height / 2), posX - 234, posY + (height / 2))
 
     def __drawSeparatorLine(self, painter):
-        self.__pen.setColor(self.__inactiveStateColor)
-        self.__pen.setWidth(1)
-        painter.setPen(self.__pen)
+        pen = QPen(self.__inactiveStateColor, 1, Qt.SolidLine)
+        painter.setPen(pen)
         painter.drawLine(10, 159, 1270, 159)
 
     def toNextState(self):
         self.__state = self.__state.next()
-        #TODO return new state
+        return self.__state
 
     def toPreviousState(self):
         self.__state = self.__state.previous()
-        #TODO return new state
+        return self.__state
 
     class WIZARD_PROGRESS_STATES(IntEnum):
         WELCOME = 0
-        SETUP_SSH = 1
-        SETUP_ACCOUNTS = 2
+        NETWORK = 1
+        ACCOUNTS = 2
         SUMMARY = 3
 
         def next(self):
             if self.value == 3:
                 raise ValueError('Enumeration ended')
-            return WIZARD_PROGRESS_STATES(self.value + 1)
+            return WizardProgressWidget.WIZARD_PROGRESS_STATES(self.value + 1)
 
         def previous(self):
             if self.value == 0:
                 raise ValueError('Enumeration ended')
-            return WIZARD_PROGRESS_STATES(self.value - 1)
+            return WizardProgressWidget.WIZARD_PROGRESS_STATES(self.value - 1)
+
+        def toDisplayValue(self):
+            ENUM_DISPLAY_VALUES = ['Welcome', 'Network', 'Accounts', 'Summary']
+            return ENUM_DISPLAY_VALUES[self.value]
 
 
-class WelcomeWidget(QWidget):
+class FirstStartWizardMiddleWidget(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setAttribute(Qt.WA_StyledBackground)
-        self.setStyleSheet("background-image: url(./view/assets/Presentation1.png);background-repeat:no-repeat;background-position:center")
+        self.setStyleSheet("background-image:url(./view/assets/Presentation1.png);background-repeat:no-repeat;background-position:center")
+        self.setFixedSize(1280, 480)
+
+    def __setup(self):
+        raise NotImplementedError('Derived class must implement method "__setup"')
+
+
+class WelcomeWidget(FirstStartWizardMiddleWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.__setup()
 
     def __setup(self):
-        self.setFixedSize(1280, 480)
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 50, 0, 0)
         layout.setSpacing(30)
         layout.setAlignment(Qt.AlignTop)
+
         welcomeLabel = QLabel("Welcome to CryptStorePi!")
-        welcomeLabelFont = QFont('Arial', 42, QFont.Normal)
+        welcomeLabelFont = QFont('Helvetica', 42, QFont.Normal)
         welcomeLabelFont.setUnderline(True)
         welcomeLabel.setFont(welcomeLabelFont)
+
         welcomeInstructionsLabel = QLabel("This wizard will guide you through the first setup of this application.")
-        welcomeInstructionsLabel.setFont(QFont('Arial', 22, QFont.Normal))
+        welcomeInstructionsLabel.setFont(QFont('Helvetica', 22, QFont.Normal))
         continueInstructionLabel = QLabel("To start, click 'Next'!")
-        continueInstructionLabel.setFont(QFont('Arial', 16, QFont.Normal))
+        continueInstructionLabel.setFont(QFont('Helvetica', 16, QFont.Normal))
 
         layout.addWidget(welcomeLabel, 0, Qt.AlignHCenter)
         layout.addWidget(welcomeInstructionsLabel, 0, Qt.AlignHCenter)
         layout.addWidget(continueInstructionLabel, 0, Qt.AlignHCenter)
         self.setLayout(layout)
 
+
+class SetupNetworkWidget(FirstStartWizardMiddleWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__setup()
+
+    def __setup(self):
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 50, 0, 0)
+        layout.setSpacing(0)
+        layout.setAlignment(Qt.AlignTop)
+
+        headingLabel = QLabel("<h3>Setup network connections</h3>")
+        
+        layout.addWidget(headingLabel, 0, Qt.AlignHCenter)
+        self.setLayout(layout)
